a3eb857611401f61120b176a67aaac3e1f97ef49
tripleo1 <50311859+tripleo1@users.noreply.github.com>
2026-08-25 15:01:17 -0400

    cegont 2025.04.21
    Skill issue or dumb sheit or laziness? Choose two.

---
+++ cegont-upper/src/main/java/tripleo/util/buffer/internal/NotImplementedException.java
@@ -0,0 +1,11 @@
+package tripleo.util.buffer.internal;
+
+public class NotImplementedException extends RuntimeException {
+   public NotImplementedException() {
+      boolean y = true;
+   }
+
+   public static void raise() {
+      boolean y = true;
+   }
+}
